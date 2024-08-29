983abcda71c47df2c939fbd57aefa0e1687f8ca0
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2024-08-29 11:23:59 +0530

    add google oauth
    Implement google oauth using next-auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,6 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth/next";
+import { authOptions } from "./options";
 
-export const authOptions = {
-    // Configure one or more authentication providers
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        // ...add more providers here
-    ],
-}
+const nextAuth = NextAuth(authOptions);
 
-export default NextAuth(authOptions)
+export { nextAuth as GET, nextAuth as POST };
